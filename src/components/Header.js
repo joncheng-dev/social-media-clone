b2e1d8c7a1d8b390e38102347640bc7e830f3fe1
Jonathan Cheng <jcheng461@gmail.com>
2022-08-23 23:01:02 +0900

    add buttons and search bar for header. add to style sheet

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,22 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <h1>Title</h1>
+      <div class="topnav">
+        <a class="active" href="#home">
+          Home
+        </a>
+        <a href="#about">Notifications</a>
+        <a href="#contact">Messages</a>
+        <input type="text" placeholder="Search.."></input>
+      </div>
+      {/* <div className="btn-group">
+        <button>Home</button>
+        <button>Notifications</button>
+        <button>Messages</button>
+      </div>
+      <div className="">
+
+      </div> */}
     </div>
   );
 }
