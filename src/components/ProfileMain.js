f4935dcb015dda4483bd3bc11145a39b4afa83da
Jonathan Cheng <jcheng461@gmail.com>
2022-08-25 23:19:43 +0900

    WIP: update style for profile-main component

--- src/components/ProfileMain.js
+++ src/components/ProfileMain.js
@@ -4,6 +4,12 @@ function ProfileMain() {
   return (
     <div className="profile-main">
       <h3>Profile Main</h3>
+      <div className="profile-main-buttons">
+        <button>one</button>
+        <button>two</button>
+        <button>three</button>
+      </div>
+      <br />
     </div>
   );
 }
