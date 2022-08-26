8c6f50da976dfa9bc7b0a6599f3549d680c4bf37
Jonathan Cheng <jcheng461@gmail.com>
2022-08-26 23:54:45 +0900

    WIP: update style for profile-main component. Splits top and bottom into two separate divs

--- src/components/ProfileMain.js
+++ src/components/ProfileMain.js
@@ -3,13 +3,17 @@ import React from "react";
 function ProfileMain() {
   return (
     <div className="profile-main">
-      <h3>Profile Main</h3>
-      <div className="profile-main-buttons">
-        <button>one</button>
-        <button>two</button>
-        <button>three</button>
+      <div className="profile-main-top">
+        <h3>Profile Main</h3>
       </div>
-      <br />
+      <div className="profile-main-bottom">
+        <div className="profile-main-buttons">
+          <button>one</button>
+          <button>two</button>
+          <button>three</button>
+        </div>
+        <br />
+      </div>
     </div>
   );
 }
