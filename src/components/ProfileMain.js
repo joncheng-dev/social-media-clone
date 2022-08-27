a0c5a3cd90e25bd7d85b754fc7b3a2c45fa59d38
Jonathan Cheng <jcheng461@gmail.com>
2022-08-27 18:12:21 +0900

    WIP: update style for profile main with profile picture, centered buttons

--- src/components/ProfileMain.js
+++ src/components/ProfileMain.js
@@ -1,13 +1,19 @@
 import React from "react";
+import defaultProfileImage from "./../img/defaultProfileImage.jpg";
 
 function ProfileMain() {
   return (
     <div className="profile-main">
       <div className="profile-main-top">
-        <h3>Profile Main</h3>
+        <div className="profile-main-picture">
+          <img src={defaultProfileImage} alt="default profile image" />
+        </div>
       </div>
       <div className="profile-main-bottom">
-        <div>
+        <div className="profile-main-name">
+          <strong>Profile Owner</strong>
+        </div>
+        <div className="profile-main-buttons-row">
           <button className="profile-main-buttons">TWEETS</button>
           <button className="profile-main-buttons">FOLLOWING</button>
           <button className="profile-main-buttons">FOLLOWERS</button>
